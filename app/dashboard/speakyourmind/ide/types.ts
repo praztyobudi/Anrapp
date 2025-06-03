108b3dbfe70ad7f492916da996d004946f48fc3a
praztyobudi <praztyob@gmail.com>
2025-06-03 10:14:59 +0700

    update speak your mind #03

---
+++ app/dashboard/speakyourmind/ide/types.ts
@@ -0,0 +1,15 @@
+export type Idea = {
+    id: number;
+    from: string;
+    to: string;
+    idea: string;
+    date: string;
+  };
+  
+  export type ApiIdea = {
+    id: number;
+    name: string;
+    department: string;
+    message: string;
+    created_at: string;
+  };
